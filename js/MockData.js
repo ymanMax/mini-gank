360fd78451e7021219a13f28738d7045d3109ae9
wangpangzhi <1356915197@qq.com>
2025-12-18 14:03:59 +0800

    init by wangpangzhi

---
+++ js/MockData.js
@@ -0,0 +1,194 @@
+/**
+ * Mock数据模块
+ * 根据页面展示逻辑生成模拟数据
+ */
+
+// 模拟数据类型
+const mockTypes = ['Android', 'iOS', '休息视频', '福利', '拓展资源', '前端', '瞎推荐', 'App'];
+
+// 生成随机字符串
+function generateRandomString(length) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
+// 生成随机日期
+function generateRandomDate() {
+  const start = new Date(2020, 0, 1);
+  const end = new Date();
+  const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  return date.toISOString().split('T')[0];
+}
+
+// 生成图片URL
+function generateImageUrl() {
+  const imageUrls = [
+    'https://picsum.photos/400/300?random=1',
+    'https://picsum.photos/400/300?random=2',
+    'https://picsum.photos/400/300?random=3',
+    'https://picsum.photos/400/300?random=4',
+    'https://picsum.photos/400/300?random=5'
+  ];
+  return imageUrls[Math.floor(Math.random() * imageUrls.length)];
+}
+
+// 生成文章URL
+function generateArticleUrl() {
+  const articleUrls = [
+    'https://github.com/daimajia/AndroidSwipeLayout',
+    'https://github.com/ReactiveX/RxJava',
+    'https://github.com/facebook/react',
+    'https://github.com/vuejs/vue',
+    'https://github.com/tensorflow/tensorflow'
+  ];
+  return articleUrls[Math.floor(Math.random() * articleUrls.length)];
+}
+
+// 生成描述文本
+function generateDescription(type) {
+  const descriptions = {
+    'Android': [
+      'Android开发必备的实用工具库',
+      '最新的Android开发框架介绍',
+      'Android性能优化技巧分享',
+      'Android UI设计最佳实践'
+    ],
+    'iOS': [
+      'iOS开发中的Swift技巧',
+      'Objective-C与Swift混合编程',
+      'iOS应用架构设计模式',
+      'iOS性能监控工具推荐'
+    ],
+    '前端': [
+      'Vue.js 3.0新特性详解',
+      'React Hooks使用指南',
+      'Webpack配置优化技巧',
+      'TypeScript在项目中的应用'
+    ],
+    '福利': [
+      '程序员必备的壁纸合集',
+      '技术分享会精彩瞬间',
+      '开发者社区活动照片',
+      '技术团队建设活动'
+    ],
+    '休息视频': [
+      '编程技巧分享视频',
+      '技术讲座录像',
+      '开发者访谈视频',
+      '编程教学视频'
+    ],
+    '拓展资源': [
+      '开发者工具推荐',
+      '编程学习资源汇总',
+      '技术文档整理',
+      '开源项目推荐'
+    ],
+    '瞎推荐': [
+      '有趣的编程小技巧',
+      '开发者必备的软件',
+      '提高效率的工具',
+      '技术书籍推荐'
+    ],
+    'App': [
+      '实用的移动应用推荐',
+      '开发工具类App介绍',
+      '效率提升App合集',
+      '技术学习App推荐'
+    ]
+  };
+  
+  const typeDescs = descriptions[type] || descriptions['瞎推荐'];
+  return typeDescs[Math.floor(Math.random() * typeDescs.length)];
+}
+
+// 生成模拟数据
+class MockData {
+  constructor() {
+    this.mockDataCache = {};
+  }
+
+  // 生成单个数据项
+  generateItem(type = 'all', index = 0) {
+    const actualType = type === 'all' ? mockTypes[Math.floor(Math.random() * mockTypes.length)] : type;
+    const isImage = Math.random() > 0.5;
+    
+    return {
+      desc: `${generateDescription(actualType)} - 模拟数据${index + 1}`,
+      url: isImage ? generateImageUrl() : generateArticleUrl(),
+      publishedAt: generateRandomDate(),
+      type: isImage ? 'img' : 'other',
+      who: `mock_user_${generateRandomString(6)}`,
+      _id: `mock_${Date.now()}_${index}`
+    };
+  }
+
+  // 生成数据列表
+  generateList(options = {}) {
+    const {
+      type = 'all',
+      num = 10,
+      page = 1
+    } = options;
+
+    const cacheKey = `${type}_${num}_${page}`;
+    
+    // 使用缓存避免重复生成相同数据
+    if (this.mockDataCache[cacheKey]) {
+      return this.mockDataCache[cacheKey];
+    }
+
+    const results = [];
+    const startIndex = (page - 1) * num;
+    
+    for (let i = 0; i < num; i++) {
+      results.push(this.generateItem(type, startIndex + i));
+    }
+
+    this.mockDataCache[cacheKey] = results;
+    return results;
+  }
+
+  // 模拟搜索数据
+  generateSearchResults(query, options = {}) {
+    const {
+      num = 10,
+      page = 1
+    } = options;
+
+    const results = [];
+    const startIndex = (page - 1) * num;
+    
+    for (let i = 0; i < num; i++) {
+      const item = this.generateItem('all', startIndex + i);
+      // 在描述中添加搜索关键词
+      item.desc = `[搜索:${query}] ${item.desc}`;
+      results.push(item);
+    }
+
+    return results;
+  }
+
+  // 模拟提交响应
+  generateSubmitResponse(data) {
+    return {
+      error: false,
+      msg: '提交成功！(模拟数据)',
+      data: {
+        ...data,
+        id: `mock_submit_${Date.now()}`,
+        createdAt: new Date().toISOString()
+      }
+    };
+  }
+
+  // 清空缓存
+  clearCache() {
+    this.mockDataCache = {};
+  }
+}
+
+module.exports = new MockData();
--- js/RequestData.js
+++ js/RequestData.js
@@ -1,5 +1,7 @@
 var options = "";
 var rootUrl = 'http://gank.io/api/';
+var MockData = require('./MockData.js');
+
 var jonit = {
     "common" : function (options){
         return encodeURI(rootUrl + "data/" + options.value + "/" + options.num + '/' + options.page);
@@ -13,6 +15,21 @@ var runjonit = function(options){
     return jonit[options.type](options);
 }
 
+// 检查网络状态
+function checkNetworkStatus() {
+  return new Promise((resolve) => {
+    wx.getNetworkType({
+      success: function(res) {
+        // 如果网络类型为none，则使用mock数据
+        resolve(res.networkType !== 'none');
+      },
+      fail: function() {
+        // 获取网络状态失败时也使用mock数据
+        resolve(false);
+      }
+    });
+  });
+}
 
 var RequestData = function(){
 
@@ -29,12 +46,26 @@ RequestData.prototype.init = function(option, callback){
         _options[key] = option[key];
     }
     options = _options;
-    this._requestD(runjonit(options), function(res){
-            if(res.data && res.data.results){
-                _self._handleData(res.data.results, callback)
-            }
-    })
     
+    // 检查网络状态，决定是否使用mock数据
+    checkNetworkStatus().then((hasNetwork) => {
+        if (hasNetwork) {
+            // 有网络时使用真实接口
+            this._requestD(runjonit(options), function(res){
+                if(res.data && res.data.results){
+                    _self._handleData(res.data.results, callback)
+                } else {
+                    // 接口返回异常时使用mock数据
+                    console.log('接口返回异常，使用mock数据');
+                    _self._handleMockData(options, callback);
+                }
+            })
+        } else {
+            // 无网络时使用mock数据
+            console.log('无网络连接，使用mock数据');
+            _self._handleMockData(options, callback);
+        }
+    });
 }
 
 RequestData.prototype._handleData = function(data, callback){
@@ -55,6 +86,39 @@ RequestData.prototype._handleData = function(data, callback){
     })
     // console.table(callData)
     callback(callData);
+}
+
+// 处理mock数据
+RequestData.prototype._handleMockData = function(options, callback) {
+    var callData = [];
+    var mockResults = [];
+    
+    if (options.type === 'search') {
+        mockResults = MockData.generateSearchResults(options.value, {
+            num: parseInt(options.num),
+            page: parseInt(options.page)
+        });
+    } else {
+        mockResults = MockData.generateList({
+            type: options.value,
+            num: parseInt(options.num),
+            page: parseInt(options.page)
+        });
+    }
+    
+    mockResults.forEach(function(value, index, arr){
+        var d = {};
+        d.desc = value.desc;
+        d.type = value.type;
+        d.url = value.url;
+        d.publishedAt = value.publishedAt;
+        callData.push(d);
+    });
+    
+    // 模拟网络延迟
+    setTimeout(function() {
+        callback(callData);
+    }, 300);
 }
 
 RequestData.prototype._requestD = function(url, callback){
